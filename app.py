391b0a816a1e7a7112eecf13672895efcd13b09f
Amr Khaled <150189549+AmrPyTh@users.noreply.github.com>
2025-02-22 13:26:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,25 +3,34 @@ import sklearn
 import helper
 import pickle
 import nltk
+import os
+
 
 nltk.download('punkt')
 nltk.download('stopwords')
 
-model = pickle.load(open('model.pkl', 'rb'))
-vectorizer = pickle.load(open('vectorizer.pkl', 'rb'))
+
+model_path = os.path.join(os.getcwd(), 'model.pkl')
+vectorizer_path = os.path.join(os.getcwd(), 'vectorizer.pkl')
+
+model = pickle.load(open(model_path, 'rb'))
+vectorizer = pickle.load(open(vectorizer_path, 'rb'))
+
 
 title = "<h1 style='text-align: center; color: green; white-space: nowrap;'>Semntimant Analysis App using ML</h1>" 
 st.markdown(title, unsafe_allow_html=True)
 text = st.text_input('Please Enter Your Review')
 state = st.button('Predict')
-token = helper.preprocessing_step(text)
-vectorized_data = vectorizer.transform([token])
+# token = helper.preprocessing_step(text)
+# vectorized_data = vectorizer.transform([token])
 
-prediction = model.predict(vectorized_data)
+# prediction = model.predict(vectorized_data)
 if state:
+    token = helper.preprocessing_step(text)
+    vectorized_data = vectorizer.transform([token])
+    prediction = model.predict(vectorized_data)
+
     if prediction == 1:
-        text_1 = "<h3 style='text-align: center; color: green; white-space: nowrap;'>Positive Review</h3>" 
-        st.markdown(text_1, unsafe_allow_html=True)
+        st.markdown("<h3 style='text-align: center; color: green;'>Positive Review</h3>", unsafe_allow_html=True)
     else:
-        text_2 = "<h3 style='text-align: center; color: green; white-space: nowrap;'>Negative Review</h3>" 
-        st.markdown(text_2, unsafe_allow_html=True)
+        st.markdown("<h3 style='text-align: center; color: red;'>Negative Review</h3>", unsafe_allow_html=True)
